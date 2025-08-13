ca7c219bd7e530d1a69abebeea6f68696f51b1d5
RI Nirob Sarkar <chocqn2580@gmail.com>
2025-08-13 14:53:32 +0600

    Create ProductPage..js

---
+++ src/pages/ProductPage..js
@@ -0,0 +1,13 @@
+// src/pages/ProductPage.js
+import React from 'react';
+
+const ProductPage = () => {
+  return (
+    <div>
+      <h1>Product Page</h1>
+      <p>This is a placeholder Product Page.</p>
+    </div>
+  );
+};
+
+export default ProductPage;
